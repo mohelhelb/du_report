602acceac6b36ab88ea7c7a481d4341a9a16296a
Mohamed El Hammouti El Berkani <mohelhelb@gmail.com>
2022-02-28 03:33:41 +0100

    launch (GitHub)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# du_report
+### Disk Space Usage
+
+#### Description
+
+This Bash script is intended as a utility that helps monitor the disk space usage for specific directories in the user's file system.
+
+#### Setup
+
+The steps that should be taken to set up this script are as follows:
+
+- Clone the GitHub repository (preferably into */home/"user"/projects/*).
+        ```
+        [mkdir -p ~/projects/]
+        git clone git@github.com:mohelhelb/du_report.git [~/projects/du_report/]
+        ```
+- Create the Disk Space Usage Directory (DSUD) and Configuration File (DSUCF).
+	```
+	mkdir -p ~/disk_usage/
+	touch ~/disk_usage/config_file.txt
+	```
+- Modify the *du_dir*, *du_rpt*, and *du_config_file* variables in the *du_report.sh* script accordingly if required.
+- Write the full path of the directories to be monitored to the DSUCF.
+- Execute the script.
+        ```
+        [sudo chmod a+x ~/projects/du_report/du_report.sh]
+        bash ~/projects/du_report/du_report.sh
+	```
---
+++ du_report.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Disk Space Usage
+##################
+
+# set -eux
+
+# --> Script functions
+# Output directories
+function display_dirs {
+	local directories="$@"
+	local directory
+	for directory in "${directories[@]}"
+	do
+		printf "%s\n" ${directory}
+	done
+}
+# <-- Script functions
+# Get user
+user=$(whoami)
+# Disk Space Usage Directory (DSUD)
+du_dir="/home/${user}/disk_usage/"
+# Disk Space Usage Report File (DSURF)
+du_rpt="${du_dir}/du.rpt"
+# Disk Space Usage Configuration File (DSUCF)
+du_config_file="${du_dir}/config_file.txt"
+# Get date-related parameters
+day=$(date +%d)
+month=$(date +%B)
+year=$(date +%Y)
+# Create array of invalid directories
+invalid_dirs=()
+# Create DSUD, if it does not exist
+mkdir -p ${du_dir}
+# Exit script if DSUCF does not exist or is empty
+if [[ ! -f ${du_config_file} ]] || [[ ! -s ${du_config_file} ]]; then
+	exit
+fi
+# Redirect STOUT to DSURF
+exec 1> ${du_rpt}
+# Print header
+printf "# ${month} ${day} ${year}:\t\tTop 10 Disk Space Usage\n\n"
+# Suppress empty entries and duplicate ones in DSUCF. Print top 10 disk space usage for each valid directory
+while read entry
+do
+	if [[ -d ${entry} ]]; then
+		entry=$(realpath ${entry})
+		printf "Directory: %s\n" ${entry}
+		du -S "${entry}" |
+		sort -nr |
+		head -n 10 |
+		cat -n |
+		gawk 'BEGIN{printf "No\tSize\t\tSubdirectory\n"}{printf "%02d:\t%d\t\t%s\n", $1,$2,$3}END{printf "\n"}'
+	else
+		invalid_dirs+=("${entry}")
+	fi
+done < <(sed "/^$/d ; s:^~/:${HOME}/: ; s:/$::" ${du_config_file} | sort -u)
+# Print invalid entries in DSUCF
+printf "Invalid Directories:\n"
+display_dirs "${invalid_dirs[@]}"
